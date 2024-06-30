63d923cda2bbb558651f8c445acdc9f183b205f7
gudangcoding <dientrimedia@gmail.com>
2024-06-30 14:28:25 +0700

    aktifitas

--- php/apps/modul/aktifitas/index.php
+++ php/apps/modul/aktifitas/index.php
@@ -10,15 +10,26 @@
     </thead>
     <tbody>
         <?php
-        $query = $konek->query("SELECT aktifitas.*, 
-                                user.username 
-                                FROM aktifitas,user 
-                                WHERE aktifitas.user_id=user.id; ");
+        //buat view aktifitas berdasarkan user yang login
+        // session_start();
+        $id = $_SESSION['id'];
+        if (@$_SESSION['username'] === "admin") {
+            $query = $konek->query("SELECT aktifitas.*, 
+                                    user.username 
+                                    FROM aktifitas
+                                    INNER JOIN user ON aktifitas.user_id=user.id ");
+        } else {
+            $query = $konek->query("SELECT aktifitas.*, 
+            user.username as name
+            FROM aktifitas
+            INNER JOIN user ON aktifitas.user_id=user.id 
+            WHERE aktifitas.user_id=$id");
+        }
         foreach ($query as $hasil) {
             echo "<tr>
                     <td>$hasil[id]</td>
                     <td>$hasil[nama_aktifitas]</td>
-                    <td>$hasil[username]</td>
+                    <td>$hasil[name]</td>
                     <td>$hasil[waktu]</td>
                 </tr>";
         }
