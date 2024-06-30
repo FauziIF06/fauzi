3b906445b9bc931b434aaca602ba0b07c909c1a7
gudangcoding <dientrimedia@gmail.com>
2024-06-30 13:29:20 +0700

    tes 5

--- php/index.php
+++ php/index.php
@@ -2,7 +2,7 @@
 session_start();
 
 // Menghindari redirect setelah login berhasil ketika sudah ada session
-if ($_SESSION["username"] == "") {
+if ($_SESSION['username'] === "") {
     header("Location: login.php");
     exit;
 }
@@ -25,20 +25,19 @@ if ($_SESSION["username"] == "") {
         <th>Nama</th>
         <th>Aksi</th>
     </tr>
-    <?php 
+    <?php
     include("koneksi.php");
 
     $query = $konek->query("SELECT * FROM kategori");
     $nomor = 1;
-    foreach ($query as $row) { 
+    foreach ($query as $row) {
         echo "<tr>";
-        echo "<td>".$nomor."</td>";
-        echo "<td>".$row["name"]."</td>";
-        echo "<td><a href='edit.php?id=$row[id]"."'>Edit</a></td>";
-        echo "<td><a href='hapus.php?id=$row[id]"."'>Hapus</a></td>";
+        echo "<td>" . $nomor . "</td>";
+        echo "<td>" . $row["name"] . "</td>";
+        echo "<td><a href='edit.php?id=$row[id]" . "'>Edit</a></td>";
+        echo "<td><a href='hapus.php?id=$row[id]" . "'>Hapus</a></td>";
         echo "</tr>";
         $nomor++;
     }
     ?>
 </table>
-
